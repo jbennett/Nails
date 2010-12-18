530bd66cfe1284887060a6886dafd390818fdd0b
Jonathan Bennett <jbennett@ccistudios.com>
2010-12-18 18:03:32 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsForNooku::Application.config.secret_token = 'f5b7af89ebd4a976291aedace7aa0f05a5c48a600968817d6dd12d124e5be5f7ee9866bd261377c0e51df9afae96cdda616ea2f0376b0c21fbd13be6f204f80e'
